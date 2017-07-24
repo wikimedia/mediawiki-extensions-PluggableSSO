e44875c3fba467b453a210b23fd76f226aeebc7b
Antoine Musso <hashar@free.fr>
2017-07-24 10:23:59 +0200

    build: add json-lint / banana i18n checker
    Add builders based on the list in the BoilerPlate extension.
    
    Document i18n message "pluggablesso-extensionname"
    
    Change-Id: Ia50ccaf49d4d1b64ffb72525c3662eb1065f09ab

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+
+	grunt.initConfig( {
+		banana: conf.MessagesDirs,
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
